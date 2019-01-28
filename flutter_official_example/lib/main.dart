fb259a12661b170ec0d878933b9303aaf0c14c67
林伟达 <284247180@qq.com>
2019-01-28 16:50:14 +0800

    upload flutter official project

---
+++ flutter_official_example/lib/main.dart
@@ -0,0 +1,417 @@
+
+// 第3步: 添加一个 有状态的部件（Stateful widget）
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return (new Text(wordPair.asPascalCase));
+  }
+}
+
+
+// // 第4步: 创建一个无限滚动ListView
+
+// import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Startup Name Generator',
+//       home: new RandomWords(),
+//     );
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   createState() => new RandomWordsState();
+// }
+
+// class RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+
+//   final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Startup Name Generator'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+
+//   Widget _buildSuggestions() {
+//     return new ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: (context, i) {
+//         if (i.isOdd) return new Divider();
+
+//         final index = i ~/ 2;
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       },
+//     );
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     return new ListTile(
+//       title: new Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//     );
+//   }
+// }
+
+
+// // 第5步: 添加交互
+
+// import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Startup Name Generator',
+//       home: new RandomWords(),
+//     );
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   createState() => new RandomWordsState();
+// }
+
+// class RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+
+//   final _saved = new Set<WordPair>();
+
+//   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Startup Name Generator'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+
+//   Widget _buildSuggestions() {
+//     return new ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: (context, i) {
+//         if (i.isOdd) return new Divider();
+
+//         final index = i ~/ 2;
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       },
+//     );
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     final alreadySaved = _saved.contains(pair);
+//     return new ListTile(
+//       title: new Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//       trailing: new Icon(
+//         alreadySaved ? Icons.favorite : Icons.favorite_border,
+//         color: alreadySaved ? Colors.red : null,
+//       ),
+//       onTap: () {
+//         setState(
+//           () {
+//             if (alreadySaved) {
+//               _saved.remove(pair);
+//             } else {
+//               _saved.add(pair);
+//             }
+//           },
+//         );
+//       },
+//     );
+//   }
+// }
+
+
+
+// // 第6步: 导航到新页面
+
+// import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Startup Name Generator',
+//       home: new RandomWords(),
+//     );
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   createState() => new RandomWordsState();
+// }
+
+// class RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+
+//   final _saved = new Set<WordPair>();
+
+//   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Startup Name Generator'),
+//         actions: <Widget>[
+//           new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+//         ],
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+
+//   Widget _buildSuggestions() {
+//     return new ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: (context, i) {
+//         if (i.isOdd) return new Divider();
+
+//         final index = i ~/ 2;
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       },
+//     );
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     final alreadySaved = _saved.contains(pair);
+//     return new ListTile(
+//       title: new Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//       trailing: new Icon(
+//         alreadySaved ? Icons.favorite : Icons.favorite_border,
+//         color: alreadySaved ? Colors.red : null,
+//       ),
+//       onTap: () {
+//         setState(
+//           () {
+//             if (alreadySaved) {
+//               _saved.remove(pair);
+//             } else {
+//               _saved.add(pair);
+//             }
+//           },
+//         );
+//       },
+//     );
+//   }
+
+//   void _pushSaved() {
+//     Navigator.of(context).push(
+//       new MaterialPageRoute(
+//         builder: (context) {
+//           final tiles = _saved.map(
+//             (pair) {
+//               return new ListTile(
+//                 title: new Text(
+//                   pair.asPascalCase,
+//                   style: _biggerFont,
+//                 ),
+//               );
+//             },
+//           );
+//           final divided = ListTile
+//               .divideTiles(
+//                 context: context,
+//                 tiles: tiles,
+//               )
+//               .toList();
+
+//           return new Scaffold(
+//             appBar: new AppBar(
+//               title: new Text('Saved Suggestions'),
+//             ),
+//             body: new ListView(children: divided),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
+// // 第7步：使用主题更改UI
+
+// import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Startup Name Generator',
+//       theme: new ThemeData(
+//         primaryColor: Colors.white,
+//       ),
+//       home: new RandomWords(),
+//     );
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   createState() => new RandomWordsState();
+// }
+
+// class RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+
+//   final _saved = new Set<WordPair>();
+
+//   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Startup Name Generator'),
+//         actions: <Widget>[
+//           new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+//         ],
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+
+//   Widget _buildSuggestions() {
+//     return new ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: (context, i) {
+//         if (i.isOdd) return new Divider();
+
+//         final index = i ~/ 2;
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       },
+//     );
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     final alreadySaved = _saved.contains(pair);
+//     return new ListTile(
+//       title: new Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//       trailing: new Icon(
+//         alreadySaved ? Icons.favorite : Icons.favorite_border,
+//         color: alreadySaved ? Colors.red : null,
+//       ),
+//       onTap: () {
+//         setState(
+//               () {
+//             if (alreadySaved) {
+//               _saved.remove(pair);
+//             } else {
+//               _saved.add(pair);
+//             }
+//           },
+//         );
+//       },
+//     );
+//   }
+
+//   void _pushSaved() {
+//     Navigator.of(context).push(
+//       new MaterialPageRoute(
+//         builder: (context) {
+//           final tiles = _saved.map(
+//                 (pair) {
+//               return new ListTile(
+//                 title: new Text(
+//                   pair.asPascalCase,
+//                   style: _biggerFont,
+//                 ),
+//               );
+//             },
+//           );
+//           final divided = ListTile
+//               .divideTiles(
+//             context: context,
+//             tiles: tiles,
+//           )
+//               .toList();
+
+//           return new Scaffold(
+//             appBar: new AppBar(
+//               title: new Text('Saved Suggestions'),
+//             ),
+//             body: new ListView(children: divided),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
